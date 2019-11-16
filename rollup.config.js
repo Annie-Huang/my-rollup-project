5d3d7d72d4dca7bdf9db8955124df9fc198b9535
Annie Huang <annie.hn@gmail.com>
2019-11-16 16:38:20 +1100

    Use rollup-plugin-terser to create another minified version of bundle.min.js with iife and named it 'version'.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,25 @@
 // rollup.config.js
+
 import json from 'rollup-plugin-json';
+// add a second minified output. As format, we choose iife.
+// This format wraps the code so that it can be consumed via a script tag in the browser while avoiding
+// unwanted interactions with other code. As we have an export, we need to provide the name of a global variable
+// that will be created by our bundle so that other code can access our export via this variable.
+import {terser} from 'rollup-plugin-terser';
 
 export default {
     input: 'src/main.js',
-    output: {
-        file: 'bundle.js',
-        format: 'cjs'
-    },
+    output: [
+        {
+            file: 'bundle.js',
+            format: 'cjs'
+        },
+        {
+            file: 'bundle.min.js',
+            format: 'iife',
+            name: 'version',
+            plugins: [terser()]
+        }
+    ],
     plugins: [ json() ]
 };
