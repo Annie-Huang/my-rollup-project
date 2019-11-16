ed2b92515ff098f3628916d2d5ce44616ca96b42
Annie Huang <annie.hn@gmail.com>
2019-11-16 16:00:22 +1100

    use ES5 to import json file directly and use rollup-plugin-json at during the build.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+// // src/main.js
+// import foo from './foo.js';
+//
+// export default function () {
+//     console.log(foo);
+// }
+
 // src/main.js
-import foo from './foo.js';
+import { version } from '../package.json';
+
 export default function () {
-    console.log(foo);
+    console.log('version ' + version);
 }
